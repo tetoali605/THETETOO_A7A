ae80288545e42787c0ca98d05c182112e4d7ed7f
cc122cc <djzeoamn3@bareed.ws>
2016-10-28 04:08:26 +0300

    Create gf.lua

---
+++ plugins/gf.lua
@@ -0,0 +1,18 @@
+local function run(msg, matches)
+  if matches[1] == "جلب ملف" then
+    local file = matches[2]
+    if is_sudo(msg) then 
+      local receiver = get_receiver(msg)
+      send_document(receiver, "./plugins/"..file..".lua", ok_cb, false)
+      else 
+        return nil
+    end
+  end
+end
+
+return {
+  patterns = {
+  "^(جلب ملف) (.*)$"
+  },
+  run = run
+}
